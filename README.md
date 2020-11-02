e435b60d03fa62776d1eac347def4eb5801ad683
Vitaly Snitovets <v.snitovets@gmail.com>
2020-11-02 15:48:03 +0200

    - added license files - changing github ci files

---
+++ README.md
@@ -0,0 +1,7 @@
+This library is used with combination of web3.js https://www.npmjs.com/package/web3
+
+Ethereum addresses observer is the library of poonkt.io exchange
+
+### Disclaimer
+
+Library isn't well tested and possibly could contain bugs, **use it in production on your own risk**
--- src/lib/addresses-observer.ts
+++ src/lib/addresses-observer.ts
@@ -1,3 +1,17 @@
+/* 
+eth-address-observer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+eth-address-observer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with eth-address-observer.  If not, see <https://www.gnu.org/licenses/>.
+*/
 /**
  * @file addresses-observer.ts
  * @author Vitaly Snitovets <v.snitovets@gmail.com>
