7e39cd73171e233f5c26c0dbd498a66e8e78e50f
Vitaly Snitovets <v.snitovets@gmail.com>
2020-12-05 21:34:03 +0200

    test fix

--- src/__tests__/performance.test.ts
+++ src/__tests__/performance.test.ts
@@ -75,12 +75,13 @@ async function detectionTest(
 	for (let i = 0; i < listSize; i++) {
 		await new Promise((resolve) => {
 			setTimeout(async () => {
-				web3.eth.sendTransaction({
-					from: coinbaseAddress,
-					to: recipients[i],
-					value: web3.utils.toWei("1", "ether")
-				});
-				resolve();
+				resolve(
+					web3.eth.sendTransaction({
+						from: coinbaseAddress,
+						to: recipients[i],
+						value: web3.utils.toWei("1", "ether")
+					})
+				);
 			}, transactionDelay);
 		});
 	}
